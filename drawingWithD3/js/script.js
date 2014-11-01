3c9c7fc6c6585a949453f3c499483ac531af04f3
Bryce Kerley <bkerley@brycekerley.net>
2014-11-01 14:52:53 -0400

    Automated commit at 2014-11-01 18:52:53 UTC by middleman-deploy 0.3.0

--- drawingWithD3/js/script.js
+++ drawingWithD3/js/script.js
@@ -3,7 +3,7 @@
 
   datasette = [6, 48, 31, 39, 5, 17, 5, 46, 37, 22];
 
-  d3.select('body').selectAll('div').data(datasette).enter().append('div').attr('class', 'bar').style('height', function(datum, index) {
+  d3.select('#example').selectAll('#example div').data(datasette).enter().append('div').attr('class', 'bar').style('height', function(datum, index) {
     return "" + (datum * 4) + "px";
   }).style('background-color', function(datum, index) {
     return "rgb(" + (4 * datum) + ", " + (25 * index) + ", " + (15 * index) + ")";
